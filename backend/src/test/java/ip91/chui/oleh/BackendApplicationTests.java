ff12acf60c5301ed8d660c22cb495ed9e01d7b48
Oleh Chui <misterchuy21@gmail.com>
2023-02-05 12:10:11 +0200

    BE: init the skeleton of the backend.

---
+++ backend/src/test/java/ip91/chui/oleh/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ip91.chui.oleh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
