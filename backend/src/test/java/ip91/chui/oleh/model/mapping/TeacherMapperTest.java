453869d85e07bbb43eca2eaf0d902b2cafb2c806
Chui Oleh <misterchuy21@gmail.com>
2023-03-04 10:42:28 +0200

    BE: add naming convention for test, do small refactor of previous tests and write for Room mapper

--- backend/src/test/java/ip91/chui/oleh/model/mapping/TeacherMapperTest.java
+++ backend/src/test/java/ip91/chui/oleh/model/mapping/TeacherMapperTest.java
@@ -3,23 +3,21 @@ package ip91.chui.oleh.model.mapping;
 import ip91.chui.oleh.model.dto.TeacherDto;
 import ip91.chui.oleh.model.entity.Teacher;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
 class TeacherMapperTest {
 
-  private final static String JOHN_DOE = "John Doe";
+  private static final String JOHN_DOE = "John Doe";
+  private static final int MAX_HOURS_PER_WEEK = 20;
 
   private final TeacherMapper teacherMapper = Mappers.getMapper(TeacherMapper.class);
 
   @Test
-  void testMapTeacherToDto() {
-    Teacher teacher = new Teacher(1L, JOHN_DOE, null, 20);
+  void Should_ConvertTeacherToDto_When_TeacherIsValid() {
+    Teacher teacher = new Teacher(1L, JOHN_DOE, null, MAX_HOURS_PER_WEEK);
 
     TeacherDto teacherDto = teacherMapper.teacherToDto(teacher);
 
@@ -30,8 +28,8 @@ class TeacherMapperTest {
   }
 
   @Test
-  void testMapDtoToTeacherWithId() {
-    TeacherDto teacherDto = new TeacherDto(1L, JOHN_DOE, null, 20);
+  void Should_ConvertDtoToTeacher_When_DtoHasId() {
+    TeacherDto teacherDto = new TeacherDto(1L, JOHN_DOE, null, MAX_HOURS_PER_WEEK);
     Teacher teacher = teacherMapper.dtoToTeacher(teacherDto);
 
     assertEquals(1L, teacher.getId());
@@ -41,20 +39,14 @@ class TeacherMapperTest {
   }
 
   @Test
-  void testMapDtoToTeacherWithoutId() {
-    TeacherDto teacherDto = new TeacherDto(null, JOHN_DOE, null, 20);
+  void Should_ConvertDtoToTeacher_When_DtoHasNotId() {
+    TeacherDto teacherDto = new TeacherDto(null, JOHN_DOE, null, MAX_HOURS_PER_WEEK);
     Teacher teacher = teacherMapper.dtoToTeacher(teacherDto);
 
     assertNull(teacher.getId());
     assertEquals(teacherDto.getName(), teacher.getName());
     assertNull(teacher.getSubjects());
     assertEquals(teacherDto.getMaxHoursPerWeek(), teacher.getMaxHoursPerWeek());
-  }
-
-  @Test
-  void testDtoToTeacherWithNull() {
-    Teacher teacher = teacherMapper.dtoToTeacher(null);
-    assertNull(teacher);
   }
 
 }
