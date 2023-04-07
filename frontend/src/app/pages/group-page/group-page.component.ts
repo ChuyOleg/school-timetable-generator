ef1636d68d14cb57d12f98f4794b298c2bd47426
Chui Oleh <misterchuy21@gmail.com>
2023-04-07 12:12:36 +0300

    frontend: start group page component

--- frontend/src/app/pages/group-page/group-page.component.ts
+++ frontend/src/app/pages/group-page/group-page.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GroupService } from "../../services/group/group.service";
+import { GroupModalService } from "../../services/group/group-modal.service";
 
 @Component({
   selector: 'app-group-page',
   templateUrl: './group-page.component.html'
 })
-export class GroupPageComponent {
+export class GroupPageComponent implements OnInit {
+
+  loading: boolean = false
+  term = ''
+
+  constructor(
+    public groupService: GroupService,
+    public modalService: GroupModalService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.groupService.getAll().subscribe(() => {
+      this.loading = false;
+    })
+  }
 
 }
