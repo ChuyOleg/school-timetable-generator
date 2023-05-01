0942ed91c3d2cc56480bef08693b5776f8ef50bb
Chui Oleh <misterchuy21@gmail.com>
2023-05-01 22:52:50 +0300

    backend: update TimeTable service and controller with create functionality

--- backend/src/main/java/ip91/chui/oleh/service/TimeTableService.java
+++ backend/src/main/java/ip91/chui/oleh/service/TimeTableService.java
@@ -2,7 +2,11 @@ package ip91.chui.oleh.service;
 
 import ip91.chui.oleh.algorithm.EvolutionaryAlgorithm;
 import ip91.chui.oleh.model.dto.lightweigth.TimeTableDtoLightWeight;
+import ip91.chui.oleh.model.entity.TimeTable;
+import ip91.chui.oleh.model.entity.User;
 import ip91.chui.oleh.model.mapping.TimeTableDtoLightWeightMapper;
+import ip91.chui.oleh.repository.TimeTableRepository;
+import ip91.chui.oleh.service.auth.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +14,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TimeTableService {
 
-  private final TimeTableDtoLightWeightMapper timeTableDtoLightWeightMapper;
-
   private final EvolutionaryAlgorithm algorithm;
+  private final AuthenticationService authService;
+  private final TimeTableRepository timeTableRepository;
+  private final TimeTableDtoLightWeightMapper timeTableDtoLightWeightMapper;
 
   public TimeTableDtoLightWeight generate() {
     return timeTableDtoLightWeightMapper.toTimeTableDtoLightWeight(algorithm.generate());
+  }
+
+  public TimeTableDtoLightWeight create(TimeTableDtoLightWeight timeTableDtoLightWeight) {
+    User user = authService.extractPrincipalFromSecurityContextHolder();
+
+    TimeTable timeTableToSave = timeTableDtoLightWeightMapper.toTimeTable(timeTableDtoLightWeight);
+    timeTableToSave.setUser(user);
+    TimeTable savedTimeTable = timeTableRepository.save(timeTableToSave);
+
+    return timeTableDtoLightWeightMapper.toTimeTableDtoLightWeight(savedTimeTable);
   }
 
 }
