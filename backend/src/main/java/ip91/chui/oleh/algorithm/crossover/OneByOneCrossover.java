090358c78ee77c8a1614a8200ae11dc6ffd6de2a
Chui Oleh <misterchuy21@gmail.com>
2023-05-01 18:33:11 +0300

    backend: implement OneByOneCrossover and update TimetableChromosomeController

---
+++ backend/src/main/java/ip91/chui/oleh/algorithm/crossover/OneByOneCrossover.java
@@ -0,0 +1,60 @@
+package ip91.chui.oleh.algorithm.crossover;
+
+import ip91.chui.oleh.algorithm.model.Individual;
+import ip91.chui.oleh.model.dto.GroupDto;
+import ip91.chui.oleh.model.dto.LessonDto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class OneByOneCrossover implements Crossover {
+
+
+  @Override
+  public List<Individual> process(List<Individual> individuals) {
+    List<Individual> offspring = new ArrayList<>();
+
+    for (int indNum = 0; indNum < individuals.size(); indNum += 2) {
+      Individual parent_1 = individuals.get(indNum);
+      Individual parent_2 = individuals.get(indNum + 1);
+
+      Individual individual1 = new Individual(new Object[parent_1.getChromosome().length]);
+      Individual individual2 = new Individual(new Object[parent_1.getChromosome().length]);
+
+      for (int geneNum = 0; geneNum < parent_1.getChromosome().length; geneNum++) {
+        if (geneNum % 2 == 0) {
+          setGeneFromParentToChild(parent_1, individual1, geneNum);
+          setGeneFromParentToChild(parent_2, individual2, geneNum);
+        } else {
+          setGeneFromParentToChild(parent_2, individual1, geneNum);
+          setGeneFromParentToChild(parent_1, individual2, geneNum);
+        }
+      }
+
+      offspring.add(individual1);
+      offspring.add(individual2);
+    }
+
+    return offspring;
+  }
+
+  // TODO: maybe creating a new Group is redundant, so mutation create new Lesson (think)
+  private void setGeneFromParentToChild(Individual parent, Individual child, int geneNum) {
+    GroupDto group = (GroupDto) parent.getChromosome()[geneNum];
+    child.getChromosome()[geneNum] = GroupDto.builder()
+        .id(group.getId())
+        .gradeNumber(group.getGradeNumber())
+        .shift(group.getShift())
+        .lessons(group.getLessons()
+            .stream()
+            .map(l -> new LessonDto(
+                l.getId(), group, l.getTeacherDto(), l.getSubjectDto(), l.getRoomDto(), l.getTimeSlotDto())
+            )
+            .collect(Collectors.toSet())
+        )
+        .build();
+  }
+}
--- backend/src/main/java/ip91/chui/oleh/algorithm/crossover/chromosomeController/TimetableChromosomeController.java
+++ backend/src/main/java/ip91/chui/oleh/algorithm/crossover/chromosomeController/TimetableChromosomeController.java
@@ -1,11 +1,14 @@
 package ip91.chui.oleh.algorithm.crossover.chromosomeController;
 
 import ip91.chui.oleh.algorithm.model.Individual;
+import ip91.chui.oleh.model.dto.GroupDto;
+import ip91.chui.oleh.model.dto.LessonDto;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class TimetableChromosomeController implements ChromosomeController {
@@ -13,13 +16,35 @@ public class TimetableChromosomeController implements ChromosomeController {
   @Override
   public Object[] createUsingPointCrossover(Individual parent1, Individual parent2, int point) {
     List<Object> child = new ArrayList<>();
-    List<Object> firstPart = Arrays.asList(parent1.getChromosome()).subList(0, point);
-    List<Object> secondPart = Arrays.asList(parent2.getChromosome()).subList(point, parent2.getChromosome().length);
+
+    List<Object> parent1Part = Arrays.asList(parent1.getChromosome()).subList(0, point);
+    List<Object> parent2Part = Arrays.asList(parent2.getChromosome()).subList(point, parent2.getChromosome().length);
+
+    List<Object> firstPart = createPartOfChild(parent1Part);
+    List<Object> secondPart = createPartOfChild(parent2Part);
 
     child.addAll(firstPart);
     child.addAll(secondPart);
 
     return child.toArray();
+  }
+
+  private List<Object> createPartOfChild(List<Object> parentGenes) {
+    return parentGenes.stream().map(gene -> {
+      GroupDto group = (GroupDto) gene;
+      return GroupDto.builder()
+          .id(group.getId())
+          .gradeNumber(group.getGradeNumber())
+          .shift(group.getShift())
+          .lessons(group.getLessons()
+              .stream()
+              .map(l -> new LessonDto(
+                  l.getId(), group, l.getTeacherDto(), l.getSubjectDto(), l.getRoomDto(), l.getTimeSlotDto())
+              )
+              .collect(Collectors.toSet())
+          )
+          .build();
+    }).collect(Collectors.toList());
   }
 
 }
