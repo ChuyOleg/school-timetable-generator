f8abb552c89aa00e84a68e2b9d4acad513caa023
Chui Oleh <misterchuy21@gmail.com>
2023-05-02 14:08:13 +0300

    backend: create new timetable-related exceptions

---
+++ backend/src/main/java/ip91/chui/oleh/exception/TimeTableDtoLightWeightValidationException.java
@@ -0,0 +1,13 @@
+package ip91.chui.oleh.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class TimeTableDtoLightWeightValidationException extends RuntimeException {
+
+  public TimeTableDtoLightWeightValidationException(String message) {
+    super(message);
+  }
+
+}
