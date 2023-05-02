f8714d639d2bcd3c807cb0f885fa89fbe53db363
Chui Oleh <misterchuy21@gmail.com>
2023-05-02 14:11:12 +0300

    backend: create TimeTableRepository

--- backend/src/main/java/ip91/chui/oleh/repository/TimeTableRepository.java
+++ backend/src/main/java/ip91/chui/oleh/repository/TimeTableRepository.java
@@ -4,6 +4,11 @@ import ip91.chui.oleh.model.entity.TimeTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
+
+  Optional<TimeTable> findByUserId(Long userId);
+
 }
