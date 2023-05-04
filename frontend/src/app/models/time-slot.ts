786806d80c20d359222abe6a4ef694408fb9f4fb
Chui Oleh <misterchuy21@gmail.com>
2023-05-04 17:41:05 +0300

    frontend: fix time-slot model

--- frontend/src/app/models/time-slot.ts
+++ frontend/src/app/models/time-slot.ts
@@ -2,7 +2,7 @@ import { EDayOfWeek } from "./enumeration/day-of-week";
 import { EWeekType } from "./enumeration/week-type";
 
 export interface ITimeSlot {
-  id?: null
+  id?: number
   lessonNumber: number
   day: EDayOfWeek
   weekType: EWeekType
