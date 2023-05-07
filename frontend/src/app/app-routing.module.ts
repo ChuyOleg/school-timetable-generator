8500dc100a36ab7c2b8486f712eadbe789ae583f
Chui Oleh <misterchuy21@gmail.com>
2023-05-07 21:56:30 +0300

    frontend: remove 'About' page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SubjectPageComponent } from "./pages/subject-page/subject-page.component";
-import { AboutPageComponent } from "./pages/about-page/about-page.component";
 import { TeacherPageComponent } from "./pages/teacher-page/teacher-page.component";
 import { RoomPageComponent } from "./pages/room-page/room-page.component";
 import { GroupPageComponent } from "./pages/group-page/group-page.component";
@@ -14,13 +13,12 @@ const routes: Routes = [
   { path: 'registration', component: RegisterPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'subjects', component: SubjectPageComponent },
-  { path: 'about', component: AboutPageComponent },
   { path: 'teachers', component: TeacherPageComponent },
   { path: 'rooms', component: RoomPageComponent },
   { path: 'groups', component: GroupPageComponent },
   { path: 'groups/:id', component: GroupDetailsPageComponent },
   { path: 'timetable', component: TimetablePageComponent },
-  { path: '**', redirectTo: '/about' }
+  { path: '**', redirectTo: '/subjects' }
 ];
 
 @NgModule({
