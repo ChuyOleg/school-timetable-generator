088f0d9fe35f2de41134907be70435f6e27f32f3
Chui Oleh <misterchuy21@gmail.com>
2024-11-28 20:15:55 +0200

    backend: refactor algorithm-related stuff

--- backend/src/main/java/ip91/chui/oleh/algorithm/model/RuntimeInfo.java
+++ backend/src/main/java/ip91/chui/oleh/algorithm/model/RuntimeInfo.java
@@ -2,14 +2,23 @@ package ip91.chui.oleh.algorithm.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RuntimeInfo {
 
+  @Setter
   private Individual bestIndividual;
   private int bestIndividualNotChangeCounter;
 
+  public void incrementBestIndividualNotChangeCounter() {
+    bestIndividualNotChangeCounter += 1;
+  }
+
+  public void resetBestIndividualNotChangeCounter() {
+    bestIndividualNotChangeCounter = 0;
+  }
 }
---
+++ backend/src/main/java/ip91/chui/oleh/config/algorithm/AlgorithmConfig.java
@@ -0,0 +1,40 @@
+package ip91.chui.oleh.config.algorithm;
+
+import ip91.chui.oleh.algorithm.Algorithm;
+import ip91.chui.oleh.algorithm.EvolutionaryAlgorithm;
+import ip91.chui.oleh.algorithm.crossover.Crossover;
+import ip91.chui.oleh.algorithm.fitnessFunction.FitnessFunction;
+import ip91.chui.oleh.algorithm.generationReplacement.GenerationReplacement;
+import ip91.chui.oleh.algorithm.mutation.Mutation;
+import ip91.chui.oleh.algorithm.mutation.utils.ProblematicLessonFinder;
+import ip91.chui.oleh.algorithm.populationGenerator.PopulationGenerator;
+import ip91.chui.oleh.algorithm.selection.Selection;
+import ip91.chui.oleh.config.properties.algorithm.AlgorithmComponentsProperties;
+import ip91.chui.oleh.config.properties.algorithm.AlgorithmConfigProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({
+    AlgorithmComponentsProperties.class, AlgorithmConfigProperties.class })
+public class AlgorithmConfig {
+
+  @Bean
+  public Algorithm evolutionaryAlgorithm(
+      AlgorithmConfigProperties config, AlgorithmComponentsProperties props,
+      FitnessFunction fitnessFunction, ApplicationContext context, ProblematicLessonFinder finder) {
+
+    PopulationGenerator populationGenerator =
+        (PopulationGenerator) context.getBean(props.getPopulationGeneratorName());
+    Selection selection = (Selection) context.getBean(props.getSelectionName());
+    Crossover crossover = (Crossover) context.getBean(props.getCrossoverName());
+    Mutation mutation = (Mutation) context.getBean(props.getMutationName());
+    GenerationReplacement generationReplacement =
+        (GenerationReplacement) context.getBean(props.getGenerationReplacementName());
+
+    return new EvolutionaryAlgorithm(config, populationGenerator,
+        selection, crossover, mutation, fitnessFunction, generationReplacement, finder);
+  }
+}
